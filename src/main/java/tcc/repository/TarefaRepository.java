3e8ff0b59be1beae8e9d1ba885f7f904f7796a53
Messeias <oseias.rdgs@gmail.com>
2017-10-25 22:02:27 -0200

    Teste

--- src/main/java/tcc/repository/TarefaRepository.java
+++ src/main/java/tcc/repository/TarefaRepository.java
@@ -12,4 +12,5 @@ public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 	@Query("SELECT t FROM Tarefa t INNER JOIN t.materia m ON m.codMateria = t.materia WHERE m.codMateria IN (SELECT c.materia.codMateria FROM Convite c WHERE c.usuario.codUsuario = ?)")
 	List<Tarefa> findByAluno(Long idAluno);
 
+	//teste
 }
